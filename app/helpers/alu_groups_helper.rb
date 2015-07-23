61adbb6e1594f36a12ba4bec0ede04ef8bf393ca
root <beevageeva@gmail.com>
2015-07-23 13:56:11 +0100

    fuzzy

--- app/helpers/alu_groups_helper.rb
+++ app/helpers/alu_groups_helper.rb
@@ -1,2 +1,7 @@
 module AluGroupsHelper
+
+def getStudentGrade(student_id, course_id)
+	gf = TestAluMsgqualif.includes(test: {work: :node}).where({student_id: student_id, nodes: {course_id: course_id}}).average(:grade_total)
+	return gf.nil? ? 0 : (gf * 10.0 / 3.0)
+end
 end
