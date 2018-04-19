40ed1a0b3db7768f17020408edf01a202485cb3e
usuario <usuario@proysiena.ull.es>
2018-04-19 23:41:32 +0100

    media de todas las notas en grupo ver

--- app/helpers/alu_groups_helper.rb
+++ app/helpers/alu_groups_helper.rb
@@ -1,7 +1,7 @@
 module AluGroupsHelper
 
-def getStudentGrade(student_id, course_id)
-	gf = TestAluMsgqualif.includes(test: {work: :node}).where({student_id: student_id, nodes: {course_id: course_id}}).average(:grade_total)
+def getStudentGrade(student_id, course_id, grade)
+	gf = TestAluMsgqualif.includes(test: {work: :node}).where({student_id: student_id, nodes: {course_id: course_id}}).average(grade)
 	return gf.nil? ? 0 : gf 
 end
 end
